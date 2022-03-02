e2beec155bc4e43a9ca807a5346df9cca48535fd
tenart <tomn98@live.unc.edu>
2022-03-02 01:08:52 -0500

    Added dynamic experience generation from JSON

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 // GLOBAL VARIABLES
+// PROJECTS
 const URL_PROJECTS_JSON = "/js/projects.json";
 const URL_PROJECT_THUMBNAILS = "/img/project-thumbnails";
 const ID_GENERAL_CONTAINER = "#general-projects";
 const ID_MOBILE_CONTAINER = "#mobile-projects";
 const ID_EXPERIMENTAL_CONTAINER = "#experimental-projects";
 const ID_OTHER_CONTAINER = "#other-projects";
+// EXPERIENCE
+const URL_EXPERIENCES_JSON = "/js/experiences.json";
+const ID_WORK_CONTAINER = "#work-experiences";
+const ID_INTERNSHIP_CONTAINER = "#internship-experiences";
+const ID_VOLUNTEER_CONTAINER = "#volunteer-experiences";
 
 // Read projects.json from URL, start appending on callback
 const getProjects = (url) => {
     $.getJSON(url, (json) => {
         appendProjects(json);
+    })
+}
+
+// Read experiences.json from URL, start appending on callback
+const getExperiences = (url) => {
+    $.getJSON(url, (json) => {
+        appendExperiences(json);
     })
 }
 
@@ -36,6 +49,25 @@ const appendProjects = (json) => {
     
 }
 
+// Appends experience entries to page
+const appendExperiences = (json) => {
+    // Extract project categories
+    const workExperiences = json.work;
+    const internshipExperiences = json.internships;
+    const volunteerExperiences = json.volunteering;
+    // Append projects by category
+    workExperiences.forEach((experience) => {
+        $(ID_WORK_CONTAINER).append(createExperienceEntryHTML(experience));
+    });
+    internshipExperiences.forEach((experience) => {
+        $(ID_INTERNSHIP_CONTAINER).append(createExperienceEntryHTML(experience));
+    });
+    volunteerExperiences.forEach((experience) => {
+        $(ID_VOLUNTEER_CONTAINER).append(createExperienceEntryHTML(experience));
+    });
+    
+}
+
 // Generate HTML for project tile from JSON data
 const createProjectTileHTML = (project) => {
     const name = project.name;
@@ -58,9 +90,67 @@ const createProjectTileHTML = (project) => {
     return $tileHTML;
 }
 
+// Generate HTML for experience entry from JSON data
+const createExperienceEntryHTML = (experience) => {
+    const title = experience.title;
+    const employer = experience.employer;
+    const year_from = experience.from;
+    const year_to = experience.to;
+    const location = experience.location;
+    const description = experience.description;
+    const url = experience.url;
+
+    const $entryHTML = $(`
+        <div class="experience-entry flex-row">
+            <div>
+                <div class="experience-dash"></div>
+            </div>
+            <div class="experience-content">
+                <h3>
+                    ${title}
+                </h3>
+                <div class="experience-info">
+                    <div class="flex-row info-row">
+                        <img src="/img/ri-briefcase.svg"/>
+                        <p>
+                            <a href="${url}" target="_blank">
+                                ${employer}
+                            </a>
+                        </p>
+                    </div>
+                    <div class="flex-row info-row">
+                        <img src="/img/ri-calendar.svg"/>
+                        <div class="flex-row year-row">
+                            ${year_from === year_to ? `
+                                <p>${year_from}</p>
+                            ` : `
+                                <p>${year_from}</p>
+                                <img src="/img/ri-arrow-right.svg"/>
+                                <p>${year_to}</p>
+                            `}
+                            
+                        </div>
+                    </div>
+                    <div class="flex-row info-row">
+                        <img src="/img/ri-location.svg"/>
+                        <p>${location}</p>
+                    </div>
+                    <div class="flex-row info-row">
+                        <img src="/img/ri-info.svg"/>
+                        <p>${description}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+    return $entryHTML;
+}
+
 // ON PAGE READY
 $(function() {
     // Read projects from JSON and render tiles
     getProjects(URL_PROJECTS_JSON);
+    // Read experiences from JSON and render entries
+    getExperiences(URL_EXPERIENCES_JSON);
 
 });
